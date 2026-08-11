4b1ebac789cfdf8635b3f54d79e5117c9759c391
Aman Sahu <amansahuxy@gmail.com>
2026-08-12 01:04:44 +0530

    Create Student object using parameterized constructor

--- App.java
+++ App.java
@@ -2,18 +2,22 @@ public class App {
 
     public static void main(String[] args) {
         // System.out.println("Hello world!");
-        Student A = new Student();
-        A.id = 1;
-        A.age = 14;
-        A.name = "Aman";
-        A.nos = 5;
-        System.out.println(A.name);
-        System.out.println(A.age);
-        System.out.println(A.id);
-        System.out.println(A.nos);
+        // Student A = new Student();
+        // A.id = 1;
+        // A.age = 14;
+        // A.name = "Aman";
+        // A.nos = 5;
+        // System.out.println(A.name);
+        // System.out.println(A.age);
+        // System.out.println(A.id);
+        // System.out.println(A.nos);
 
-        A.bunk();
-        A.study();
-        A.sleep();
+        // A.bunk();
+        // A.study();
+        // A.sleep();
+
+        // parameterized constructor called
+
+        Student A = new Student(1, 12, "Rahul", 3);
     }
 }
