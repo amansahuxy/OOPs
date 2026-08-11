63dfe90160f14194f8541420940851d83da10f31
Aman Sahu <amansahuxy@gmail.com>
2026-08-12 00:42:19 +0530

    add study method

--- Student.java
+++ Student.java
@@ -1,5 +1,13 @@
 public class Student {
     // Attributes
     public int id;
+    public int age;
+    public String name;
+    public int nos;
+
+    // Methods behaviour
+    public void study() {
+        System.out.println(name + "Studying");
+    }
 
 }
