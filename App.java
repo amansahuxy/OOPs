25923ed61cdcf366bd92c2c9eea63f60c5111489
Aman Sahu <amansahuxy@gmail.com>
2026-08-12 00:55:06 +0530

    Initialize and display Student details

--- App.java
+++ App.java
@@ -3,5 +3,13 @@ public class App {
     public static void main(String[] args) {
         // System.out.println("Hello world!");
         Student A = new Student();
+        A.id = 1;
+        A.age = 14;
+        A.name = "Aman";
+        A.nos = 5;
+        System.out.println(A.name);
+        System.out.println(A.age);
+        System.out.println(A.id);
+        System.out.println(A.nos);
     }
 }
