6f3195baf2fc9883c6cf78001fd55fd938fa137c
Aman Sahu <amansahuxy@gmail.com>
2026-08-16 23:25:00 +0530

    Access private gf field using getter

--- App.java
+++ App.java
@@ -22,11 +22,11 @@ public class App {
         System.out.println(A.age);
         System.out.println(A.id);
         System.out.println(A.nos);
-        System.out.println(A.gf);
+        System.out.println(A.getgf());
         A.bunk();
         A.study();
         A.sleep();
-        A.gfChatting();
+        // A.gfChatting();
 
         // copy constructor
         // Student B = new Student(A);
